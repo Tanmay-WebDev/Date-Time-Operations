57a802bb8abaf977b6210056c4fa4518fc474630
DeveloperTanmay <umas81261@gmail.com>
2025-02-11 19:20:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# Date and Time Operation
+import datetime
+import calendar
+
+currentTime = datetime.datetime.now()
+
+print("Time now at Greenwich Meridian is : " , end= "")
+print(currentTime)
+
+print(calendar.calendar(2025))
